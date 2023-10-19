a384f7062e0151fd64d7a5da2119f3d8be60fa98
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-19 14:38:36 -0700

    WIP

---
+++ ConsoleApiCall/Models/Multimedia.cs
@@ -0,0 +1,10 @@
+namespace ApiTest
+{
+  public class Multimedia
+  { 
+    public string Type { get; set; }
+    public string SubType { get; set; }
+    public string Caption { get; set; }
+  }
+}
+
